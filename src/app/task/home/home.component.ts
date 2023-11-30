40964a58aa5038109949ca181cc8edea40f760ff
Harsh Patel <harsh3608@gmail.com>
2023-11-30 17:39:46 +0530

    pick-list done

--- src/app/task/home/home.component.ts
+++ src/app/task/home/home.component.ts
@@ -1,12 +1,172 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { Product } from '../shared/product-models';
+import { ProductService } from '../shared/product.service';
+import { PickListModule } from 'primeng/picklist';
+import { PaginatorModule } from 'primeng/paginator';
+import { log } from 'console';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [PickListModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  sourceProducts: Product[] = [
+    {
+      "id": "1000",
+      "code": "f230fh0g3",
+      "name": "Bamboo Watch",
+      "description": "Product Description",
+      "image": "bamboo-watch.jpg",
+      "price": 65,
+      "category": "Accessories",
+      "quantity": 24,
+      "inventoryStatus": "INSTOCK",
+      "rating": 5
+    },
+    {
+      "id": "1001",
+      "code": "nvklal433",
+      "name": "Black Watch",
+      "description": "Product Description",
+      "image": "black-watch.jpg",
+      "price": 72,
+      "category": "Accessories",
+      "quantity": 61,
+      "inventoryStatus": "INSTOCK",
+      "rating": 4
+    },
+    {
+      "id": "1002",
+      "code": "zz21cz3c1",
+      "name": "Blue Band",
+      "description": "Product Description",
+      "image": "blue-band.jpg",
+      "price": 79,
+      "category": "Fitness",
+      "quantity": 2,
+      "inventoryStatus": "LOWSTOCK",
+      "rating": 3
+    },
+    {
+      "id": "1003",
+      "code": "244wgerg2",
+      "name": "Blue T-Shirt",
+      "description": "Product Description",
+      "image": "blue-t-shirt.jpg",
+      "price": 29,
+      "category": "Clothing",
+      "quantity": 25,
+      "inventoryStatus": "INSTOCK",
+      "rating": 5
+    },
+    {
+      "id": "1004",
+      "code": "h456wer53",
+      "name": "Bracelet",
+      "description": "Product Description",
+      "image": "bracelet.jpg",
+      "price": 15,
+      "category": "Accessories",
+      "quantity": 73,
+      "inventoryStatus": "INSTOCK",
+      "rating": 4
+    },
+    {
+      "id": "1005",
+      "code": "av2231fwg",
+      "name": "Brown Purse",
+      "description": "Product Description",
+      "image": "brown-purse.jpg",
+      "price": 120,
+      "category": "Accessories",
+      "quantity": 0,
+      "inventoryStatus": "OUTOFSTOCK",
+      "rating": 4
+    },
+    {
+      "id": "1006",
+      "code": "bib36pfvm",
+      "name": "Chakra Bracelet",
+      "description": "Product Description",
+      "image": "chakra-bracelet.jpg",
+      "price": 32,
+      "category": "Accessories",
+      "quantity": 5,
+      "inventoryStatus": "LOWSTOCK",
+      "rating": 3
+    },
+    {
+      "id": "1007",
+      "code": "mbvjkgip5",
+      "name": "Galaxy Earrings",
+      "description": "Product Description",
+      "image": "galaxy-earrings.jpg",
+      "price": 34,
+      "category": "Accessories",
+      "quantity": 23,
+      "inventoryStatus": "INSTOCK",
+      "rating": 5
+    },
+    {
+      "id": "1008",
+      "code": "vbb124btr",
+      "name": "Game Controller",
+      "description": "Product Description",
+      "image": "game-controller.jpg",
+      "price": 99,
+      "category": "Electronics",
+      "quantity": 2,
+      "inventoryStatus": "LOWSTOCK",
+      "rating": 4
+    },
+    {
+      "id": "1009",
+      "code": "cm230f032",
+      "name": "Gaming Set",
+      "description": "Product Description",
+      "image": "gaming-set.jpg",
+      "price": 299,
+      "category": "Electronics",
+      "quantity": 63,
+      "inventoryStatus": "INSTOCK",
+      "rating": 3
+    }
+  ];
+  indexedSourceProducts: Product[] = [];
+
+  targetProducts: Product[] = [];
+
+  constructor(private carService: ProductService, private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+    // this.carService.getProductsSmall().then(
+    //   (products: Product[]) =>
+    //     this.sourceProducts = products,
+
+    // );
+
+    this.targetProducts = [];
+    this.primengConfig.ripple = true;
+    this.indexedSourceProducts = this.sourceProducts.slice(0,2);
+    console.log(this.sourceProducts);
+    console.log(this.indexedSourceProducts);
+
+  }
+
+
+  test(event: any) {
+    console.log(event);
+    this.indexedSourceProducts = (this.sourceProducts.slice(event.first, (event.first + event.rows))).filter(item => !this.targetProducts.includes(item));
+    console.log(this.indexedSourceProducts);
+
+  }
+
+
 
 }
+
+
--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TaskRoutingModule } from './task-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    TaskRoutingModule
+    TaskRoutingModule,
+    HttpClientModule,
   ]
 })
 export class TaskModule { }
