23763b50fd1b8dda87233fc2b4a2d326bccca35c
harsh3608 <harsh3608@gmail.com>
2023-12-21 22:51:13 +0530

    advanced

--- src/app/task/home/home.component.ts
+++ src/app/task/home/home.component.ts
@@ -7,154 +7,33 @@ import { PaginatorModule } from 'primeng/paginator';
 import { log } from 'console';
 import { ChoicesService } from '../shared/choices.service';
 import { Choice } from '../shared/choices';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PickListModule, PaginatorModule],
+  imports: [PickListModule, PaginatorModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  sourceProducts: Product[] = [
-    {
-      "id": "1000",
-      "code": "f230fh0g3",
-      "name": "Bamboo Watch",
-      "description": "Product Description",
-      "image": "bamboo-watch.jpg",
-      "price": 65,
-      "category": "Accessories",
-      "quantity": 24,
-      "inventoryStatus": "INSTOCK",
-      "rating": 5
-    },
-    {
-      "id": "1001",
-      "code": "nvklal433",
-      "name": "Black Watch",
-      "description": "Product Description",
-      "image": "black-watch.jpg",
-      "price": 72,
-      "category": "Accessories",
-      "quantity": 61,
-      "inventoryStatus": "INSTOCK",
-      "rating": 4
-    },
-    {
-      "id": "1002",
-      "code": "zz21cz3c1",
-      "name": "Blue Band",
-      "description": "Product Description",
-      "image": "blue-band.jpg",
-      "price": 79,
-      "category": "Fitness",
-      "quantity": 2,
-      "inventoryStatus": "LOWSTOCK",
-      "rating": 3
-    },
-    {
-      "id": "1003",
-      "code": "244wgerg2",
-      "name": "Blue T-Shirt",
-      "description": "Product Description",
-      "image": "blue-t-shirt.jpg",
-      "price": 29,
-      "category": "Clothing",
-      "quantity": 25,
-      "inventoryStatus": "INSTOCK",
-      "rating": 5
-    },
-    {
-      "id": "1004",
-      "code": "h456wer53",
-      "name": "Bracelet",
-      "description": "Product Description",
-      "image": "bracelet.jpg",
-      "price": 15,
-      "category": "Accessories",
-      "quantity": 73,
-      "inventoryStatus": "INSTOCK",
-      "rating": 4
-    },
-    {
-      "id": "1005",
-      "code": "av2231fwg",
-      "name": "Brown Purse",
-      "description": "Product Description",
-      "image": "brown-purse.jpg",
-      "price": 120,
-      "category": "Accessories",
-      "quantity": 0,
-      "inventoryStatus": "OUTOFSTOCK",
-      "rating": 4
-    },
-    {
-      "id": "1006",
-      "code": "bib36pfvm",
-      "name": "Chakra Bracelet",
-      "description": "Product Description",
-      "image": "chakra-bracelet.jpg",
-      "price": 32,
-      "category": "Accessories",
-      "quantity": 5,
-      "inventoryStatus": "LOWSTOCK",
-      "rating": 3
-    },
-    {
-      "id": "1007",
-      "code": "mbvjkgip5",
-      "name": "Galaxy Earrings",
-      "description": "Product Description",
-      "image": "galaxy-earrings.jpg",
-      "price": 34,
-      "category": "Accessories",
-      "quantity": 23,
-      "inventoryStatus": "INSTOCK",
-      "rating": 5
-    },
-    {
-      "id": "1008",
-      "code": "vbb124btr",
-      "name": "Game Controller",
-      "description": "Product Description",
-      "image": "game-controller.jpg",
-      "price": 99,
-      "category": "Electronics",
-      "quantity": 2,
-      "inventoryStatus": "LOWSTOCK",
-      "rating": 4
-    },
-    {
-      "id": "1009",
-      "code": "cm230f032",
-      "name": "Gaming Set",
-      "description": "Product Description",
-      "image": "gaming-set.jpg",
-      "price": 299,
-      "category": "Electronics",
-      "quantity": 63,
-      "inventoryStatus": "INSTOCK",
-      "rating": 3
-    }
-  ];
-  indexedSourceProducts: Product[] = [];
-  indexedTargetProducts: Product[] = [];
-  targetProducts: Product[] = [];
 
-  sourceObjects: Choice[] = [];
-  indexedObjects: Choice[] = [];
-
-  first2: number = 0;
-  rows2: number = 10;
-  options = [
-    { label: 10, value: 10 },
-    { label: 20, value: 20 },
-    { label: 50, value: 50 }
-  ];
-
-  sourceStartIndex: number = 0;
+  sourceStartIndex: number = 1;
   sourceEndIndex: number = 10;
+  sourceRows: number = 10;
+  sourceObjects: any[] = [];
+  totalCount: number = 2508;
+
+  targetStartIndex: number = 1;
+  targetEndIndex: number = 10;
+  targetRows: number = 10;
+  targetObjects: any[] = [];
+  indexedTargetObjects: any[] = [];
+  options = [
+    { label: '10', value: 10 },
+    { label: '20', value: 20 },
+    { label: '50', value: 50 }
+  ];
 
   constructor(
     private carService: ProductService,
@@ -164,23 +43,17 @@ export class HomeComponent {
 
   ngOnInit() {
 
-    this.targetProducts = [];
-    this.primengConfig.ripple = true;
-    this.indexedSourceProducts = this.sourceProducts.slice(0, 2);
-    console.log(this.sourceProducts);
-    console.log(this.indexedSourceProducts);
-
-    this.indexedObjects = this.getObjects(this.first2, this.rows2);
+    
 
   }
 
-  getObjects(index: number, rows: number) {
+  getSourceObjects(index: number, pageSize: number) {
     let objects: Choice[] = [];
-    this.objectService.getChoices(index, rows).subscribe(
+    this.objectService.getChoices(index, pageSize).subscribe(
       (res) => {
         console.log(res);
         objects = res;
-        this.indexedObjects = res;
+        this.sourceObjects = res;
         
       }
     );
@@ -188,31 +61,181 @@ export class HomeComponent {
     return objects;
   }
 
-  onPageChange2(event: any) {
-
-    this.first2 = event.first;
-    this.rows2 = event.rows;
-    this.indexedSourceProducts = (this.sourceProducts.slice(event.first, (event.first + event.rows))).filter(item => !this.targetProducts.includes(item));
-  
-  
-    //this.sourceObjects = ;
+  sourcePaginator(isFirst: boolean, isLast: boolean, isForward: boolean, isBackward: boolean) {
+    //this.getSourceObjects(sourceIndex,sourceRows);
+    if (isFirst) {
+      this.sourceStartIndex = 1;
+      this.sourceEndIndex = this.sourceRows;
+      this.getSourceObjects(this.sourceStartIndex, this.sourceRows);
+      //.sourceTracker();
+    } else if (isLast) {
+      // debugger;
+      //this.sourceStartIndex = (this.totalCount - this.targetObjects.length) - this.sourceRows;
+      let startIndex = (this.totalCount - this.targetObjects.length) - ((this.totalCount - this.targetObjects.length) % this.sourceRows);
+      if (startIndex === (this.totalCount - this.targetObjects.length)) {
+        startIndex = (this.totalCount - this.targetObjects.length) - this.sourceRows;
+      };
+      this.sourceStartIndex = startIndex + 1;
+      this.sourceEndIndex = this.totalCount - this.targetObjects.length;
+      this.getSourceObjects(this.sourceStartIndex, this.sourceRows);
+    } else if (isForward) {
+      this.sourceStartIndex = this.sourceStartIndex + this.sourceRows;
+      this.sourceEndIndex = this.sourceEndIndex + this.sourceRows;
+      this.getSourceObjects(this.sourceStartIndex, this.sourceRows);
+    } else if (isBackward) {
+      // debugger;
+      this.sourceStartIndex = this.sourceStartIndex - this.sourceRows;
+      let endIndex = this.sourceEndIndex - this.sourceRows;
+      if ((endIndex % 10) !== 0) {
+        endIndex = this.sourceEndIndex - (endIndex % 10);
+      };
+      this.sourceEndIndex = endIndex;
+      this.getSourceObjects(this.sourceStartIndex - this.sourceRows, this.sourceRows);
+    }
   }
 
+  onMoveToTarget(event: any) {
+    const movedItems = event.items;
+    this.targetObjects.push(...movedItems);
+    this.sourceTracker();
+    this.targetPaginator(true, false, false, false);
+  }
 
-  sourcePaginator(event: any) {
+  onMoveToSource(event: any) {
+    const movedItems = event.items;
+    this.targetObjects = this.targetObjects.filter(object => !movedItems.some((i: any) => i.artifact_id === object.artifact_id));
+    this.sourceTracker();
+    this.targetPaginator(true, false, false, false);
+  }
+
+  targetPaginator(isFirst: boolean, isLast: boolean, isForward: boolean, isBackward: boolean) {
+    if (isFirst) {
+      this.targetStartIndex = 1;
+      this.targetEndIndex = this.targetRows;
+      this.indexedTargetObjects = this.targetObjects.slice(0, this.targetEndIndex);
+    } else if (isLast) {
+      let startIndex = this.targetObjects.length - (this.targetObjects.length % this.targetRows);
+      if (startIndex === this.targetObjects.length) {
+        startIndex = this.targetObjects.length - this.targetRows;
+      };
+      this.targetStartIndex = startIndex + 1;
+      this.targetEndIndex = this.targetObjects.length;
+      this.indexedTargetObjects = this.targetObjects.slice(this.targetStartIndex - 1, this.targetEndIndex);
+    } else if (isForward) {
+      this.targetStartIndex = this.targetStartIndex + this.targetRows;
+      this.targetEndIndex = this.targetEndIndex + this.targetRows;
+      this.indexedTargetObjects = this.targetObjects.slice(this.targetStartIndex - 1, this.targetEndIndex);
+
+    } else if (isBackward) {
+      this.targetStartIndex = this.targetStartIndex - this.targetRows;
+      let endIndex = this.targetEndIndex - this.targetRows;
+      if ((endIndex % 10) !== 0) {
+        endIndex = this.targetEndIndex - (endIndex % 10);
+      };
+      this.targetEndIndex = endIndex;
+
+      this.indexedTargetObjects = this.targetObjects.slice(this.targetStartIndex - 1, this.targetEndIndex);
+    }
+
+  }
+
+  sourceTracker() {
     debugger;
-    console.log(event);
-    this.indexedSourceProducts = (this.sourceProducts.slice(event.first, (event.first + event.rows))).filter(item => !this.targetProducts.includes(item));
-    console.log(this.indexedSourceProducts);
+    if ( (this.sourceEndIndex === (this.totalCount - this.targetObjects.length)) ) {
+
+      this.getSourceObjects(this.sourceStartIndex, ((this.sourceEndIndex-this.sourceStartIndex)+1) );
+    } else {
+      if (this.sourceObjects.length > this.sourceRows) {
+        const newPageSize = this.sourceRows;
+        if ((this.sourceStartIndex + newPageSize) < (this.totalCount - this.targetObjects.length)) {
+          this.getSourceObjects(this.sourceStartIndex, newPageSize);
+        };
+      } else if (this.sourceObjects.length < this.sourceRows) {
+        //console.log('fetch', this.sourceRows-this.sourceObjects.length, 'more objects');
+        const newPageSize = this.sourceRows + (this.sourceRows - this.sourceObjects.length);
+        //if ((this.sourceStartIndex + newPageSize) > (this.totalCount - this.targetObjects.length)) {
+          this.getSourceObjects(this.sourceStartIndex, newPageSize);
+        //};
+      }else if(this.sourceObjects.length===0) {
+        const newPageSize = (this.totalCount)
+        this.getSourceObjects(this.sourceStartIndex, newPageSize);
+      }
+    }
+    
+    
 
   }
 
-  targetPaginator(event: any) {
-    console.log(event);
-    this.indexedTargetProducts = this.targetProducts.slice(event.first, (event.first + event.rows));
-    console.log(this.targetProducts);
+  changeSourceRows() {
+    // debugger;
+    this.sourceObjects = [];
+    // console.log(this.sourceRows);
+    // let endIndex = this.sourceStartIndex + this.sourceRows - 1
+    // this.sourceEndIndex = endIndex;
+    // this.getSourceObjects(this.sourceStartIndex, this.sourceRows);
+    this.sourceStartIndex = 1;
+    this.sourceEndIndex = this.sourceRows;
+    this.getSourceObjects(this.sourceStartIndex, this.sourceRows);
+  }
+
+  changeTargetRows() {
+    // debugger;
+    this.indexedTargetObjects = []
+    // console.log(this.targetRows);
+    this.targetStartIndex = 1;
+    let endIndex = this.targetRows;
+    this.targetEndIndex = endIndex;
+    this.indexedTargetObjects = this.targetObjects.slice(this.targetStartIndex - 1, this.targetEndIndex);
 
   }
+
+  isTargetObject(object: any) {
+    let filteredObject = this.targetObjects.filter(x => x.artifact_id === object.artifact_id);
+    if (filteredObject.length > 0) {
+      return true;
+    } else {
+      return false;
+    };
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // onPageChange2(event: any) {
+
+  //   this.first2 = event.first;
+  //   this.rows2 = event.rows;
+  //   this.indexedSourceProducts = (this.sourceProducts.slice(event.first, (event.first + event.rows))).filter(item => !this.targetProducts.includes(item));
+  
+  
+  //   //this.sourceObjects = ;
+  // }
+
+
+  // sourcePaginator(event: any) {
+  //   debugger;
+  //   console.log(event);
+  //   this.indexedSourceProducts = (this.sourceProducts.slice(event.first, (event.first + event.rows))).filter(item => !this.targetProducts.includes(item));
+  //   console.log(this.indexedSourceProducts);
+
+  // }
+
+  // targetPaginator(event: any) {
+  //   console.log(event);
+  //   this.indexedTargetProducts = this.targetProducts.slice(event.first, (event.first + event.rows));
+  //   console.log(this.targetProducts);
+
+  // }
 
 }
 
--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskRoutingModule } from './task-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     TaskRoutingModule,
     HttpClientModule,
+    ButtonModule,
   ]
 })
 export class TaskModule { }
